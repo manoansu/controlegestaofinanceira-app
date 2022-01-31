4039f6f1421cbe28c35b925abeb87ce0b7b18280
Ansumane Mané <manoansu@gmail.com>
2022-01-31 15:05:38 +0000

    Start a new Project

---
+++ controlegestaofinanceira-app/src/main/java/pt/amane/controlegestaofinanceiraapp/ControlegestaofinanceiraAppApplication.java
@@ -0,0 +1,13 @@
+package pt.amane.controlegestaofinanceiraapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlegestaofinanceiraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlegestaofinanceiraAppApplication.class, args);
+	}
+
+}
---
+++ controlegestaofinanceira-app/src/test/java/pt/amane/controlegestaofinanceiraapp/ControlegestaofinanceiraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.amane.controlegestaofinanceiraapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlegestaofinanceiraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
